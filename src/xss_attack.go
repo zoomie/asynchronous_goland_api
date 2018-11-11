bb2d6f1cf27ba5a40b634feafd8f4379130b5f91
andrwe <andrew.arderne@protonmail.com>
2018-11-11 17:19:05 +0000

    adding forms

--- src/xss_attack.go
+++ src/xss_attack.go
@@ -3,11 +3,13 @@ package main
 import (
 	"net/http"
 	"html/template"
+	"fmt"
 )
 
-func process(w http.ResponseWriter,r *http.Request) {
+func process(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
 	t, _ := template.ParseFiles("tmpl.html")
-	t.Execute(w, r.FormValue("comment"))
+	t.Execute(w, r.Form)
 }
 
 func form(w http.ResponseWriter, r *http.Request) {
@@ -16,10 +18,11 @@ func form(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	fmt.Println("up and running")
 	server := http.Server{
-		Addr : "8000"
+		Addr : "127.0.0.1:8000",
 	}
-	http.HandleFunc("/form", form) 
 	http.HandleFunc("/process", process)
+	http.HandleFunc("/", form) 
 	server.ListenAndServe()
 }
